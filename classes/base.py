65211739174281513d39ef639750ccf5cb1edffa
Venu Myneni <ielektron.myneni@daimler.com>
2023-04-30 16:16:42 +0530

    Formatting

--- classes/base.py
+++ classes/base.py
@@ -1,53 +1,80 @@
+import warnings
+from pprint import pprint as print
+
+
 class Board:
-    player_0=0
-    player_1 =1
+    player_0 = 0
+    player_1 = 1
+
     def __init__(self) -> None:
-        self.state =[[None,None,None],[None,None,None],[None,None,None]]
-        self.marks =0
-    def mark(self,player_id,x,y):
+        self.state = [[None, None, None], [None, None, None], [None, None, None]]
+        self._marks = 0
+
+    @property
+    def marks(self):
+        return self._marks
+
+    @marks.setter
+    def marks(self, value):
+        self._marks = value
+
+    def mark(self, player_id, x, y):
+        if x > 2 or y > 2:
+            warnings.warn(f"X i.e. {x} and Y i.e {y} should be <= 2")
+            return False
         if self.state[x][y] is None:
             self.state[x][y] = player_id
-            self.marks +=1
+            self.marks = self.marks + 1
             return True
         else:
+            warnings.warn(f"X i.e. {x} and Y i.e. {y} is already occupied")
             return False
-    def has_ended(self):
 
-        for row in range(0,3):
-            if (self.state[row][0] ==self.state[row][1]==self.state[row][2]) and (self.state[row][0] is not None):
-                return (True,self.state[row][0])
-        
-        for col in range(0,3):
-            if (self.state[0][col] ==self.state[1][col]==self.state[2][col]) and (self.state[0][col] is not None):
-                return (True,self.state[0][col])
-        
-        if self.state[0][0]==self.state[1][1]==self.state[2][2] and self.state[0][0] is not None:
-            return (True,self.state[0][0])
-        
-        if self.state[0][2]==self.state[1][1]==self.state[2][2] and self.state[2][0] is not None:
-            return (True,self.state[1][1])
-        
-        if self.marks ==9:
-            return (True,None)
-        
-        return (False,None)
+    def has_ended(self):
+        for row in range(0, 3):
+            if (self.state[row][0] == self.state[row][1] == self.state[row][2]) and (
+                self.state[row][0] is not None
+            ):
+                return (True, self.state[row][0])
+
+        for col in range(0, 3):
+            if (self.state[0][col] == self.state[1][col] == self.state[2][col]) and (
+                self.state[0][col] is not None
+            ):
+                return (True, self.state[0][col])
+
+        if (
+            self.state[0][0] == self.state[1][1] == self.state[2][2]
+            and self.state[0][0] is not None
+        ):
+            return (True, self.state[0][0])
+
+        if (
+            self.state[0][2] == self.state[1][1] == self.state[2][2]
+            and self.state[2][0] is not None
+        ):
+            return (True, self.state[1][1])
+
+        if self.marks == 9:
+            return (True, None)
+
+        return (False, None)
 
     def print(self) -> str:
         print("***********")
         for row in self.state:
-            print(row)
-
-
+            c = ["_" if ele is None else f"{ele}" for ele in row]
+            c = ",".join(c)
+            print(c)
 
 
 class Player:
     pass
 
+
 if __name__ == "__main__":
     board = Board()
-    board.print()
-    board.mark(0,1,2)
-    board.print()
-    board.mark(1,1,1)
-    board.print()
+    board.mark(0, 1, 3)
+    board.mark(1, 1, 1)
+    board.mark(1, 1, 1)
     print(board.has_ended())
--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 from classes.base import Board
-def single_play(board,player_id):
+
+
+def single_play(board, player_id):
     player_input = input(f"Enter Player {player_id} Mark : ")
-    x,y = player_input.split(",")
-    x,y= int(x),int(y)
+    x, y = player_input.split(",")
+    x, y = int(x), int(y)
     print(f"Player {player_id} Input : {x},{y}")
-    while not board.mark(player_id,x,y):
+    while not board.mark(player_id, x, y):
         player_input = input(f"Enter Player {player_id} Valid Mark : ")
-        x,y = player_input.split(",")
-        x,y= int(x),int(y)
+        x, y = player_input.split(",")
+        x, y = int(x), int(y)
         print(f"Player {player_id} Input : {x},{y}")
     return board
+
 
 def run_game():
     player_0 = input("Enter Player 0 Name : ")
     player_1 = input("Enter Player 1 Name : ")
-    adict = {0:player_0,1:player_1}
+    adict = {0: player_0, 1: player_1}
     board = Board()
     board.print()
-    ended,winner =board.has_ended()
+    ended, winner = board.has_ended()
     while not ended:
-
-        board = single_play(board,0)
+        board = single_play(board, 0)
         board.print()
-        ended,winner =board.has_ended()
-        print(ended,winner)
+        ended, winner = board.has_ended()
+        print(ended, winner)
         if ended:
             break
-        board = single_play(board,1)
+        board = single_play(board, 1)
         board.print()
-        ended,winner =board.has_ended()
+        ended, winner = board.has_ended()
     print(f"Winner is : {adict[winner]}")
-        
-if __name__ =="__main__":
+
+
+if __name__ == "__main__":
     run_game()
